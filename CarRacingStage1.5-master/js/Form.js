526a96324d370401dd9aedc2a9d58c2843eecfa0
Spartan1005 <73547583+Spartan1005@users.noreply.github.com>
2022-01-22 11:17:28 -0500

    Add files via upload

---
+++ CarRacingStage1.5-master/js/Form.js
@@ -0,0 +1,49 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton("reset");
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Forza 8-bit");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    //adjusts the object placement according to the devices display size
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.reset.position(displayWidth - 30, 30);
+
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Welcome " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+    this.reset.mousePressed(()=>{
+      player.updateCount(0)
+      game.update(0)
+      Player.updateCarsAtEnd(0)
+      var playerInfoRef = database.ref('players');
+      playerInfoRef.remove()
+    });
+
+  }
+}
